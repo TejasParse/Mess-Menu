82773e868a64d20287c3113abef7d1d3fa4c5107
TejasParse <tejasparse1@gmail.com>
2022-03-10 13:05:24 +0530

    Bug fixed

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
+document.getElementById('date').valueAsDate = new Date();
+getMenu(new Date());
+
+document.querySelector("#date").addEventListener("change",()=>{
+  
+    let temp = document.getElementById('date').valueAsDate;
+    if(temp==null)
+    {
+        return;
+    }
+    getMenu(temp)
+    console.log(temp.getDay());
+    console.log(temp.getDate());
+});
+
 breakfastEssen = ["WHEAT BREAD","BUTTER JAM","TEA/COFFEE/MILK"]
 lunchEssen = ["PICKLE/SUGAR/SALT"]
 snacksEssen = ["TEA/COFFEE/MILK"]
 dinnerEssen = ["PICKLE/SUGAR/SALT"]
 
-let day = new Date();
+function getMenu(dayClass)
+{
+    fetch("./menu.json")
+    .then(response=>response.json())
+    .then(data=> setMenu(data,dayClass));
+} 
 
-fetch("./menu.json")
-.then(response=>response.json())
-.then(data=> setMenu(data));
 
-function setMenu(data)
+function setMenu(data,day)
 {
     let dayNo = day.getDay();
-    let weekNo = day.getDate()/7;
-    let dayMenu = data[!weekNo%2][dayNo];
-
+    let weekNo = parseInt(day.getDate()/7);
+    let weekAns;
+    if(weekNo%2==0)
+    {
+        weekAns="odd";
+    }
+    else
+    {
+        weekAns="even";
+    }
+    console.log(dayNo,weekNo,weekAns);
+    let dayMenu = data[weekAns][dayNo];
+    
     let breakfast = document.querySelector("#breakfast");
     let breakfastMenu = [...dayMenu[0],...breakfastEssen];
 
+    console.log(breakfast.children);
+
+    while (breakfast.childNodes.length > 2) {
+        breakfast.removeChild(breakfast.lastChild);
+    }
     for(let i = 0;i<breakfastMenu.length;i++)
     {
         let obj1 = document.createElement("li");
@@ -28,7 +60,9 @@ function setMenu(data)
     
     let lunch = document.querySelector("#lunch");
     let lunchMenu = [...dayMenu[1],...lunchEssen];
-
+    while (lunch.childNodes.length > 2) {
+        lunch.removeChild(lunch.lastChild);
+    }
     for(let i = 0;i<lunchMenu.length;i++)
     {
         obj1 = document.createElement("li");
@@ -39,7 +73,9 @@ function setMenu(data)
 
     let dinner = document.querySelector("#dinner");
     let dinnerMenu = [...dayMenu[3],...dinnerEssen];
-
+    while (dinner.childNodes.length > 2) {
+        dinner.removeChild(dinner.lastChild);
+    }
     for(let i = 0;i<dinnerMenu.length;i++)
     {
         obj1 = document.createElement("li");
@@ -50,7 +86,9 @@ function setMenu(data)
 
     let snacks = document.querySelector("#snacks");
     let snacksMenu = [...dayMenu[2],...snacksEssen];
-
+    while (snacks.childNodes.length > 2) {
+        snacks.removeChild(snacks.lastChild);
+    }
     for(let i = 0;i<snacksMenu.length;i++)
     {
         obj1 = document.createElement("li");
